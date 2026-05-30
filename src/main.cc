3cfeecffff186cf304636d6c1581625c98cff707
Kenley Cheung <ken@lordkenthegreat.com>
2026-05-29 22:32:21 -0700

    feat: add GPU and XR detection logic

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #define SDL_MAIN_HANDLED
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#ifndef __APPLE__
+#include <SDL3/SDL_openxr.h>
+#endif
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <openxr/openxr.h>
@@ -10,6 +13,9 @@ using json = nlohmann::json;
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+/* These will be populated by SDL */
+XrInstance xr_instance = XR_NULL_HANDLE;
+XrSystemId xr_system_id = 0;
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -20,6 +26,22 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    SDL_PropertiesID props = SDL_CreateProperties();
+    #ifdef __APPLE__
+    SDL_SetStringProperty(props, SDL_PROP_GPU_DEVICE_CREATE_NAME_STRING, "metal");
+    #endif
+    #ifndef __APPLE__
+    SDL_SetStringProperty(props, SDL_PROP_GPU_DEVICE_CREATE_NAME_STRING, "vulkan");
+    SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_XR_ENABLE_BOOLEAN, true);
+    SDL_SetPointerProperty(props, SDL_PROP_GPU_DEVICE_CREATE_XR_INSTANCE_POINTER, &xr_instance);
+    SDL_SetPointerProperty(props, SDL_PROP_GPU_DEVICE_CREATE_XR_SYSTEM_ID_POINTER, &xr_system_id);
+    #endif
+    SDL_GPUDevice *device = SDL_CreateGPUDeviceWithProperties(props);
+    if (device == NULL) {
+        SDL_Log("Failed to create GPU device: %s", SDL_GetError());
+    }
+    SDL_DestroyProperties(props);
 
     if (!SDL_CreateWindowAndRenderer("Boson", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
